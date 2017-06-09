6b3370bc81e1a482d5021737495b6f24cff7dc53
Pavel Vlasikhin <pavel.vlasikhin@gmail.com>
2017-06-09 13:14:53 +0300

    some rubocop refactoring

--- lib/redactor2_rails.rb
+++ lib/redactor2_rails.rb
@@ -2,9 +2,9 @@ require 'redactor2_rails/version'
 require 'orm_adapter'
 
 module Redactor2Rails
-  IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'image/pjpeg', 'image/tiff', 'image/x-png']
+  IMAGE_TYPES = %w[image/jpeg image/png image/gif image/jpg image/pjpeg image/tiff image/x-png]
 
-  FILE_TYPES = ['application/msword', 'application/pdf', 'text/plain', 'text/rtf', 'application/vnd.ms-excel']
+  FILE_TYPES = %w[application/msword application/pdf text/plain text/rtf application/vnd.ms-excel]
 
   autoload :Http, 'redactor2_rails/http'
   autoload :Devise, 'redactor2_rails/devise'
@@ -18,8 +18,8 @@ module Redactor2Rails
   require 'redactor2_rails/engine'
 
   mattr_accessor :images_file_types, :files_file_types
-  @@images_file_types = ['jpg', 'jpeg', 'png', 'gif', 'tiff']
-  @@files_file_types = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'rtf', 'txt']
+  @@images_file_types = %w[jpg jpeg png gif tiff]
+  @@files_file_types = %w[pdf doc docx xls xlsx rtf txt]
 
 
   def self.image_model
--- lib/redactor2_rails/orm/base.rb
+++ lib/redactor2_rails/orm/base.rb
@@ -25,8 +25,8 @@ module Redactor2Rails
 
           def as_json(options = nil)
             options = {
-              :methods => as_json_methods,
-              :root => false
+              methods: as_json_methods,
+              root: false
             }
 
             super options
